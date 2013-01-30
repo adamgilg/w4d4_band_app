d8e3001e27730d36a67b0808bd959606ea2b8685
Shaun Mullen <smmullen90@gmail.com>
2013-01-30 09:07:18 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbMigration::Application.config.secret_token = 'ddfd2442f476d99a88cd41aa2c6f437ae9b7cc3e1ba819166f758619383cecb8d043b86a7b80f720f212ec8966819cd450afe55f888f797b5224ee5140da6103'
